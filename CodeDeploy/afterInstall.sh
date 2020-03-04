8ad2d128b620396a0ca1783006bc1de5ecd70d91
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-04 18:05:30 -0500

    shell file modified

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -3,6 +3,5 @@ cd /home/ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/environment
-sudo kill $(lsof -t -i:8000)
 echo Starting WebApplication
-nohup java -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt
+nohup java -Dserver.port=8081 -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt
