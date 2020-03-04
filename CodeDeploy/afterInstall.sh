232debb11e0144a36d66296aedf8a3ab1e979221
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-04 17:51:52 -0500

    shell file modified

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu
-sudo chown -R ubntu:ubntu /home/ubntu/
+sudo chown -R ubuntu:ubuntu /home/ubuntu/
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/environment
 kill -9 $(ps -ef|grep webApp | grep -v grep | awk '{print$2}')
