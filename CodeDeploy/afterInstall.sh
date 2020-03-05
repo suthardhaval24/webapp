4506dd71f15c63b25ed681ea3df008e40414c226
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-05 15:23:43 -0500

    shell modified

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -4,4 +4,4 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/environment
 echo Starting WebApplication on ec2
-nohup java -Dserver.port=8081 -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt &
+nohup java -Dserver.port=8081 -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt 2> /home/ubuntu/output.txt &
