bb8dbdc9a6bb0b7c0ef2ee5caf45c009d4f0c625
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-05 16:23:40 -0500

    updated afterInstall and application property

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -3,5 +3,6 @@ cd /home/ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/environment
+kill -9 $(ps -ef|grep WebApplication | grep -v grep | awk '{print$2}')
 echo Starting WebApplication on ec2
 nohup java -Dserver.port=8081 -jar WebApplication-0.0.1-SNAPSHOT.jar > /home/ubuntu/output.txt 2> /home/ubuntu/output.txt &
