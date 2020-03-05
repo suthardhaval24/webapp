9fa01cd77d1cde2bdc528d24b0a0f986e056b980
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-05 14:58:14 -0500

    appspec and shell file modified

--- CodeDeploy/afterInstall.sh
+++ CodeDeploy/afterInstall.sh
@@ -4,5 +4,4 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/
 sudo chmod +x WebApplication-0.0.1-SNAPSHOT.jar
 source /etc/environment
 echo Starting WebApplication on ec2
-kill -9 $(ps -ef|grep WebApplication | grep -v grep | awk '{print$2}')
-nohup java -Dserver.port=8081 -jar WebApplication-0.0.1-SNAPSHOT.jar &> /home/ubuntu/output.txt
+nohup java -Dserver.port=8081 -jar WebApplication-0.0.clea1-SNAPSHOT.jar > /home/ubuntu/output.txt &
