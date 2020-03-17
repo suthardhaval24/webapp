ada79058617850ab68f018144af186762b7971ed
suthardhaval24 <suthar.d@husky.neu.edu>
2020-03-16 23:05:05 -0400

    Logger modified

--- WebApplication/src/main/java/com/cloud/WebApplication.java
+++ WebApplication/src/main/java/com/cloud/WebApplication.java
@@ -1,13 +1,14 @@
 package com.cloud;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
 
-	private final static Logger log = Logger.getLogger(WebApplication.class.getName());
+	private final static Logger log = LogManager.getLogger(WebApplication.class.getName());
 	public static void main(String[] args) {
 		log.info("Starting a Web Application");
 		SpringApplication.run(WebApplication.class, args);
